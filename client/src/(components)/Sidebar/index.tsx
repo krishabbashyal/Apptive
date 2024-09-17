0b1b2622294f1e1cb1ff61d4975ca23f8cd796cb
Krishab Bashyal <krishab2002@gmail.com>
2024-09-17 14:47:22 -0500

    Cleaned the default next js app

---
+++ client/src/(components)/Sidebar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Sidebar = () => {
+  return (
+    <div className='w-64 min-h-screen bg-blue-100 flex flex-col'>
+      <div>
+        <p>sidebar</p>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <div>Just like your job</div>
+      <p>This page does not exist.</p>
+    </div>
+  )
+}
+
+export default NotFound
