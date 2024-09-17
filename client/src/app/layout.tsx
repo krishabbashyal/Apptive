b45cee3b654bbce6eef173a082a7ad032b848209
Krishab Bashyal <krishab2002@gmail.com>
2024-09-17 15:07:56 -0500

    I have no idea what is happeneing

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "@/(components)/Sidebar";
+import Sidebar from "@/components/Sidebar";
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
