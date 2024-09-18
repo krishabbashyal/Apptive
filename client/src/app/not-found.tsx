b3c5353b6555b8912584b7611bf5f5d1dc0f9c10
Krishab Bashyal <krishab2002@gmail.com>
2024-09-18 16:18:45 -0500

    Made changes to the 404 page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
-      <div>Just like your job</div>
-      <p>This page does not exist.</p>
+    <div className='flex flex-col min-h-screen justify-center items-center'>
+      <h1 className='text-5xl font-black'>Just like your job</h1>
+      <h2 className='text-3xl mt-2'>This page does not exist.</h2>
     </div>
   )
 }
