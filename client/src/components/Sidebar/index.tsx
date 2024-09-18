2a06d6d5714afba691e179ce2d08519bb95bca56
Krishab Bashyal <krishab2002@gmail.com>
2024-09-18 17:59:42 -0500

    Added icons and corrected spacing for the sidebar

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,58 +1,63 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Archive, Bug, CalendarDots, GearSix, House, IconProps, MapTrifold, SignOut } from "@phosphor-icons/react";
 
 const SidebarProfileTab = () => {
   return (
-    <div className='w-full h-24 rounded flex justify-center bg-background'>
-      <div className='flex gap-x-4 mt-3.5 items-center'>
-        <Image src="/images/profile.jpg" className='rounded-full' width={42} height={42} alt='Profile Picture' />
+    <div className="w-full h-24  flex justify-center cursor-pointer bg-background">
+      <div className="flex gap-x-4 mt-3.5 items-center">
+        <Image src="/images/profile.jpg" className="rounded-full" width={48} height={48} alt="Profile Picture" />
         <div>
           <p>Krishab Bashyal</p>
-          <p className='text-xs -mt-0.5 text-gray-400'>Developer</p>
+          <p className="text-xs -mt-0.5 text-gray-400">Developer</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 interface SidebarTabProps {
-  label: string,
-  customStyle?: string,
+  label: string;
+  customStyle?: string;
+  icon: React.ComponentType<IconProps>;
 }
 
-const SidebarTab = ({label}: SidebarTabProps) => {
+const SidebarTab = ({ label, icon: Icon, customStyle }: SidebarTabProps) => {
   return (
-    <div className='w-full h-12 rounded flex justify-center items-center bg-background'>
-      <p>{label}</p>
+    <div className={`h-12 flex  items-center bg-background cursor-pointer border-l-4 border-background ${customStyle || ""}`}>
+      <div className="flex ml-[30px] items-center">
+        <Icon color="#fff" size={24} />
+        <p className="ml-4">{label}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 const Sidebar = () => {
   return (
-    <div className='w-64 min-h-screen bg-foreground flex flex-col'>
-      <div className='flex flex-col mt-16 mx-4 flex-grow'>
+    <div className="w-64 min-h-screen bg-foreground flex flex-col">
+      <div className="flex flex-col mt-16 mx-4 flex-grow">
         <SidebarProfileTab />
-        <div className='flex mt-8 flex-col flex-grow'>
-          <div className='flex flex-col gap-y-2 flex-grow'>
-            <SidebarTab label="Dashboard"/>
-            <SidebarTab label="Timeline"/>
-            <SidebarTab label="Map"/>
-            <SidebarTab label="Archive"/>
-            <SidebarTab label="Settings"/>
+        <div className="flex mt-8 flex-col flex-grow">
+          <div className="flex flex-col gap-y-2 flex-grow">
+            <SidebarTab icon={House} label="Home" customStyle="border-blue-500"/>
+            <SidebarTab icon={CalendarDots} label="Timeline" />
+            <SidebarTab icon={MapTrifold} label="Map" />
+            <SidebarTab icon={Archive} label="Archive" />
+            <SidebarTab icon={GearSix} label="Settings" />
           </div>
           {/* Spacer div to push the bottom tabs to the bottom */}
-          <div className='flex-grow'></div>
+          <div className="flex-grow"></div>
           {/* End of comment */}
-          <div className='mb-8 flex flex-col gap-y-2'>
-            <SidebarTab label="Feedback"/>
-            <SidebarTab label="Sign out"/>
+          <div className="mb-8 flex flex-col gap-y-2">
+            <SidebarTab icon={Bug} label="Feedback" />
+            <SidebarTab icon={SignOut} label="Sign out" />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Sidebar
+export default Sidebar;
