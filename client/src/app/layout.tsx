10408f95009418102fd8efd8137027a133b2581f
Krishab Bashyal <krishab2002@gmail.com>
2024-09-18 18:53:37 -0500

    Added sidebar and navbar to global layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./globals.css";
 
+import Sidebar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
+
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Sidebar />
+        <Navbar />
         {children}
       </body>
     </html>
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Sidebar from '@/components/Sidebar'
-import Navbar from '@/components/Navbar'
 
 const Home = () => {
   return (
     <div>
-      <Sidebar />
-      <Navbar />
+
     </div>
   )
 }
