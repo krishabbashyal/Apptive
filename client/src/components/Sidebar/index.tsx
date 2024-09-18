946db2665efd7e7626f2cc9bd1fa4ed5536eb99b
Krishab Bashyal <krishab2002@gmail.com>
2024-09-18 18:30:04 -0500

    Made some small ui tweaks to sidebar

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -6,8 +6,8 @@ import { Archive, Bug, CalendarDots, GearSix, House, IconProps, MapTrifold, Sign
 
 const SidebarProfileTab = () => {
   return (
-    <div className="w-full h-24  flex justify-center cursor-pointer bg-background">
-      <div className="flex gap-x-4 mt-3.5 items-center">
+    <div className="w-full h-20 mt-4 rounded-md flex justify-center cursor-pointer bg-background">
+      <div className="flex gap-x-4 items-center">
         <Image src="/images/profile.jpg" className="rounded-full" width={48} height={48} alt="Profile Picture" />
         <div>
           <p>Krishab Bashyal</p>
@@ -26,7 +26,7 @@ interface SidebarTabProps {
 
 const SidebarTab = ({ label, icon: Icon, customStyle }: SidebarTabProps) => {
   return (
-    <div className={`h-12 flex  items-center bg-background cursor-pointer border-l-4 border-background ${customStyle || ""}`}>
+    <div className={`h-12 flex  items-center bg-background cursor-pointer border-l-4 hover:bg-slate-700 rounded-md border-background ${customStyle || ""}`}>
       <div className="flex ml-[30px] items-center">
         <Icon color="#fff" size={24} />
         <p className="ml-4">{label}</p>
@@ -40,7 +40,7 @@ const Sidebar = () => {
       <div className="flex flex-col mt-16 mx-4 flex-grow">
         <SidebarProfileTab />
         <div className="flex mt-8 flex-col flex-grow">
-          <div className="flex flex-col gap-y-2 flex-grow">
+          <div className="flex flex-col gap-y-2.5 flex-grow">
             <SidebarTab icon={House} label="Home" customStyle="border-blue-500"/>
             <SidebarTab icon={CalendarDots} label="Timeline" />
             <SidebarTab icon={MapTrifold} label="Map" />
@@ -50,7 +50,7 @@ const Sidebar = () => {
           {/* Spacer div to push the bottom tabs to the bottom */}
           <div className="flex-grow"></div>
           {/* End of comment */}
-          <div className="mb-8 flex flex-col gap-y-2">
+          <div className="mb-8 flex flex-col gap-y-2.5">
             <SidebarTab icon={Bug} label="Feedback" />
             <SidebarTab icon={SignOut} label="Sign out" />
           </div>
