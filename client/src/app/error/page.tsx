9a0266c8f8f29cda3aec9de8f5ddc5699fe96239
Krishab Bashyal <krishab2002@gmail.com>
2024-09-19 01:58:20 -0500

    Setup an error page incase any of the server components throw an error in the future

---
+++ client/src/app/error/page.tsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <p>Sorry, something went wrong D:</p>
+    </div>
+  )
+}
