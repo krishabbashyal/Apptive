ffa6b18191707b8ae9530a800791f83796a6f2a7
Krishab Bashyal <krishab2002@gmail.com>
2024-09-20 13:16:43 -0500

    Added new wrapper to layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,14 +3,13 @@ import "./globals.css";
 
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
+import DashboardWrapper from "./dashboardWrapper";
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <Sidebar />
-        <Navbar />
-        {children}
+        <DashboardWrapper> {children}</DashboardWrapper>
       </body>
     </html>
   );
