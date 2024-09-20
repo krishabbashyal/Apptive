7dfa03e61d22e648bc512f8d338ec22213b7f18a
Krishab Bashyal <krishab2002@gmail.com>
2024-09-20 13:32:32 -0500

    Removed content from navbar and moved to sidebar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -5,13 +5,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full absolute top-0 justify-center h-20 bg-foreground flex flex-col'>
-      <div>
-        <p className='text-3xl text-left font-black ml-14'>AppTrack</p>
-      </div>
-      <div>
+    <div className='w-full top-0 justify-center h-20 bg-foreground flex flex-col'>
 
-      </div>
+
+
     </div>
   )
 }
