ea5e27138cb06bc056bbdceea3d2f594e3c5d077
Krishab Bashyal <krishab2002@gmail.com>
2024-09-23 17:33:43 -0500

    Added new section for the login page

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,14 +1,17 @@
-import { login, signup } from './actions'
+import { login } from './actions'
+import AuthCard from '@/components/AuthForms/AuthCard'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className='flex min-h-screen justify-center items-center gap-x-4'>
+      <form className='flex flex-col'>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+      </form>
+      <AuthCard />  
+    </div>
   )
 }
