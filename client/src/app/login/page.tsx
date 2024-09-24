41205122a04f20155859ef6b0b16ed50cf4ca0af
Krishab Bashyal <krishab2002@gmail.com>
2024-09-24 13:48:50 -0500

    Made more changes to the login form

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -3,14 +3,14 @@ import AuthCard from '@/components/AuthForms/AuthCard'
 
 export default function LoginPage() {
   return (
-    <div className='flex min-h-screen justify-center items-center gap-x-4'>
-      <form className='flex bg-white p-8 rounded-lg flex-col'>
+    <div className='flex min-h-screen justify-center items-center w-full gap-x-4'>
+      <form className='flex bg-white p-8 rounded-lg w-96 flex-col'>
       <h1 className='text-5xl text-black font-black mb-4'>Log in</h1>
         <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={login}>Log in</button>
+        <input  className="h-12 rounded pl-4" id="email" name="email" type="email" required />
+        <label className='mt-2' htmlFor="password">Password:</label>
+        <input className="h-12 rounded pl-4" id="password" name="password" type="password" required />
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4' formAction={login}>Log in</button>
       </form>
       <AuthCard />  
     </div>
