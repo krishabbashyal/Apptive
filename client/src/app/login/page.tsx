9aa78e6c80485c42153c09c6464d79f53ee6929e
Krishab Bashyal <krishab2002@gmail.com>
2024-09-24 13:44:59 -0500

    Started designing login form

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -4,7 +4,8 @@ import AuthCard from '@/components/AuthForms/AuthCard'
 export default function LoginPage() {
   return (
     <div className='flex min-h-screen justify-center items-center gap-x-4'>
-      <form className='flex flex-col'>
+      <form className='flex bg-white p-8 rounded-lg flex-col'>
+      <h1 className='text-5xl text-black font-black mb-4'>Log in</h1>
         <label htmlFor="email">Email:</label>
         <input id="email" name="email" type="email" required />
         <label htmlFor="password">Password:</label>
