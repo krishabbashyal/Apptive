87177596a827859a9b420645d0f6a23fc75c2d8e
Krishab Bashyal <krishab2002@gmail.com>
2024-09-28 15:53:07 -0500

    Made changes the the log in and sign up forms

--- client/src/components/AuthForms/Login.tsx
+++ client/src/components/AuthForms/Login.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const LoginForm = () => {
   return (
     <form className="flex bg-foreground p-8 rounded-l-lg w-96 flex-col">
-      <h1 className="text-3xl text-white font-bold mb-4 text-center">Log in</h1>
+      <h1 className="text-3xl font-bold mb-4 text-center">Log in</h1>
       <label className="text-gray-400 font-medium" htmlFor="email">
         Email
       </label>
       <input
-        className="h-12 rounded-lg border bg-background border-gray-500 pl-4 mt-1 text-white"
+        className="h-12 rounded-lg border bg-background border-gray-500 pl-4 mt-1 "
         placeholder="Enter your email"
         id="email"
         name="email"
@@ -19,14 +19,14 @@ const LoginForm = () => {
         Password
       </label>
       <input
-        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 text-white"
+        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 "
         placeholder="Enter your password"
         id="password"
         name="password"
         type="password"
         required
       />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium h-12 py-2 px-4 rounded-lg mt-12">
+      <button className="bg-accent text-white font-medium h-12 py-2 px-4 rounded-lg mt-12">
         Log in
       </button>
     </form>
--- client/src/components/AuthForms/Signup.tsx
+++ client/src/components/AuthForms/Signup.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const SignupForm = () => {
   return (
     <form className="flex bg-foreground p-8 rounded-l-lg w-96 flex-col">
-      <h1 className="text-3xl text-white font-bold mb-4 text-center">Sign up</h1>
+      <h1 className="text-3xl font-bold mb-4 text-center">Sign up</h1>
       <label className="text-gray-400 font-medium" htmlFor="email">
         Email
       </label>
       <input
-        className="h-12 rounded-lg border bg-background border-gray-500 pl-4 mt-1 text-white"
+        className="h-12 rounded-lg border bg-background border-gray-500 pl-4 mt-1 "
         placeholder="Enter your email"
         id="email"
         name="email"
@@ -19,7 +19,7 @@ const SignupForm = () => {
         Username
       </label>
       <input
-        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 text-white"
+        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 "
         placeholder="Enter your username"
         id="username"
         name="username"
@@ -30,7 +30,7 @@ const SignupForm = () => {
         Password
       </label>
       <input
-        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 text-white"
+        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 "
         placeholder="Enter your password"
         id="password"
         name="password"
@@ -41,14 +41,14 @@ const SignupForm = () => {
         Confirm Password
       </label>
       <input
-        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 text-white"
+        className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 "
         placeholder="Confirm your password"
         id="confirmPassword"
         name="password"
         type="password"
         required
       />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium h-12 py-2 px-4 rounded-lg mt-12">
+      <button className="bg-accent hover:bg-blue-700 text-white font-medium h-12 py-2 px-4 rounded-lg mt-12">
         Sign up
       </button>
     </form>
