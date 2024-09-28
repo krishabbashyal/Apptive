fb46074376cc73ed1534781c11a195352b49992c
Krishab Bashyal <krishab2002@gmail.com>
2024-09-28 15:05:40 -0500

    Removed form from page into seperate component

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,24 +1,11 @@
-import { login } from "./actions";
+import LoginForm from "@/components/AuthForms/Login";
 import AuthCard from "@/components/AuthForms/AuthCard";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen justify-center items-center w-full gap-x-4">
-      <form className="flex bg-foreground p-8 rounded-l-lg w-96 flex-col">
-        <h1 className="text-3xl text-white font-bold mb-4 text-center">Login</h1>
-        <label className="text-gray-400 font-medium" htmlFor="email">
-          Email
-        </label>
-        <input className="h-12 rounded-lg border bg-background border-gray-500 pl-4 mt-1 text-white" placeholder="Enter your email" id="email" name="email" type="email" required />
-        <label className="mt-3 text-gray-400 font-medium" htmlFor="password">
-          Password
-        </label>
-        <input className="h-12 rounded-lg pl-4 mt-1 border bg-background border-gray-500 text-white" placeholder="Enter your password" id="password" name="password" type="password" required />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium h-12 py-2 px-4 rounded-lg mt-12" formAction={login}>
-          Login
-        </button>
-      </form>
-      <AuthCard />
-    </div>
+      <div className="flex justify-center h-full items-center w-full gap-x-4">
+        <LoginForm />
+        <AuthCard />
+      </div>
   );
 }
