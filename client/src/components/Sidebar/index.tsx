52d06853d6c16980f11f357b1edefdc2d94f5c3e
Krishab Bashyal <krishab2002@gmail.com>
2024-09-29 15:20:06 -0500

    Got the border color working again

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -42,7 +42,7 @@ const SidebarTab = ({ label, icon: Icon, route }: SidebarTabProps) => {
       onClick={() => router.push(route)}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}                                                                                                     // Adding accentLight makes the border not show up
-      className={`h-12 flex border-l-[6px] border-white items-center cursor-pointerentLight hover:text-accentLight rounded-md ${ isActiveTab ? "border-accent  text-lg text-accentLight" : ""}`}>
+      className={`h-12 flex border-l-[6px] border-white items-center cursor-pointerentLight hover:text-accentLight rounded-md ${ isActiveTab ? "border-accentLight  text-lg text-accentLight" : ""}`}>
       <div className="flex ml-[20px] items-center">
         <Icon color={isHovered || isActiveTab ? "#B8A0FF" : "black"} size={isActiveTab ? 30 : 28} />
         <p className="ml-5">{label}</p>
