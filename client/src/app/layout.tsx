d71864404ec953cbd77d60f9c6c4356509a403b3
Krishab Bashyal <krishab2002@gmail.com>
2024-10-02 03:11:35 -0500

    Added Geist font, logo image and more custom colors

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./globals.css";
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className="bg-background">{children}</body>
     </html>
   );
