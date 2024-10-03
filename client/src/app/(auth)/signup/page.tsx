14024bde97878549005dfc72d492728726f06c08
Krishab Bashyal <krishab2002@gmail.com>
2024-10-02 23:25:21 -0500

    Cleaned up sign up page

--- client/src/app/(auth)/signup/page.tsx
+++ client/src/app/(auth)/signup/page.tsx
@@ -1,11 +1,10 @@
 import SignupForm from "@/components/AuthForms/Signup";
-import AuthCard from "@/components/AuthForms/AuthCard";
+
 
 export default function LoginPage() {
   return (
       <div className="flex justify-center min-h-screen items-center w-full">
         <SignupForm />
-        <AuthCard />
       </div>
   );
 }
