b7a0fce2d2f5d496027c102f3caa58c05f6741fa
Krishab Bashyal <krishab2002@gmail.com>
2024-10-02 20:46:32 -0500

    Resolved issue with scroll

--- client/src/app/(auth)/signup/page.tsx
+++ client/src/app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import AuthCard from "@/components/AuthForms/AuthCard";
 
 export default function LoginPage() {
   return (
-      <div className="flex justify-center h-full items-center w-full">
+      <div className="flex justify-center min-h-screen items-center w-full">
         <SignupForm />
         <AuthCard />
       </div>
