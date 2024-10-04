942881079cd3a8116cd2fb3a2b3b71bbd9aed46f
Krishab Bashyal <krishab2002@gmail.com>
2024-10-04 01:44:11 -0500

    Made changes to center the login form

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -3,9 +3,8 @@ import AuthCard from "@/components/AuthForms/AuthCard";
 
 export default function LoginPage() {
   return (
-      <div className="flex justify-center h-full items-center w-full gap-x-4">
+    <div className="flex justify-center min-h-screen items-center w-full">
         <LoginForm />
-        <AuthCard />
       </div>
   );
 }
