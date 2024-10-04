cff1c492eb89f566a6f8b19d17beae39d4c433b8
Krishab Bashyal <krishab2002@gmail.com>
2024-10-04 01:32:12 -0500

    Added some new colors and custom animations

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -13,19 +13,35 @@ const config: Config = {
         foreground: "#0C0C0C",    
         accent: "#BB86FC",         
         accentLight: "#D3B4FC",    
-        accentDark: "#7D3FFF",    
+        accentDark: "#7D3FFF", 
+        cadetGrayLight: "#6A6B6C",   
         cadetGray: "#3A3B3C",      
         cadetGrayDark: "#252627",   
         graySeperator: "#2D2D2D", 
+        bittersweetLight: "#FF9D9D",
         bittersweet: "#FF6D6D",    
+        bittersweetDark: "#E05555",
       },
       fontFamily: {
         sans: ['var(--font-geist-sans)'],
         mono: ['var(--font-geist-mono)'],
       },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+          '75%': { transform: 'translateX(-5px)' },
+        },
+      },
+      animation: {
+        shake: 'shake 0.3s ease-in-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 };
 
 export default config;
