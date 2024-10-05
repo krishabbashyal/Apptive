88be7157286e714162453d0d779fb1df1bca02ae
Krishab Bashyal <krishab2002@gmail.com>
2024-10-05 09:44:58 -0500

    previous

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const Navbar = () => {
+  return (
+    <div className='w-full top-0 justify-center h-20 bg-foreground flex flex-col'>
+
+      <p>Apptive</p>
+
+    </div>
+  )
+}
+
+export default Navbar
