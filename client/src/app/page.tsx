a585c9f323bd74860fca2e087aa69e06c389be60
Krishab Bashyal <krishab2002@gmail.com>
2024-10-05 18:50:57 -0500

    worked on some stuff

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div>
       <Navbar />
-      <div className='flex flex-row h-screen w-full bg-foreground'>
+      <div className='flex flex-row h-[calc(100vh-6rem)] w-full bg-foreground'>
         <div className='w-3/5'>
           <div className='mx-32'>
             <div className='mt-16'>
@@ -14,8 +14,13 @@ const LandingPage = () => {
               <h1 className='text-8xl font-semibold'>Easily</h1>
             </div>
 
-            <p className='text-2xl font-medium mt-16 text-cadetGrayLight'>Elevate your job search with a tool that helps you stay organized and on top</p>
-            <p className='text-2xl font-medium mt-16 text-cadetGrayLight'>Track and manage all your applications in one place, eliminating the need for clunky spreadsheets. Say goodbye to endless rows and columns, and enjoy a streamlined experience designed for efficiency. </p>
+            <div>
+              <p className='text-3xl font-medium mt-10 text-cadetGrayLight'>Elevate your job search with a tool that helps you stay organized and on top</p>
+              <p className='text-2xl font-medium mt-4 text-cadetGrayLight'>Track and manage all your applications in one place, eliminating the need for clunky spreadsheets. Say goodbye to endless rows and columns, and enjoy a streamlined experience designed for efficiency. </p>
+            </div>
+            <div>
+            <button className=' text-xl bg-white text-black font-semibold px-6 py-2.5 rounded-lg mt-6'>Get started</button>
+            </div>
           </div>
         </div>
         <div className='w-2/5 bg-background'>
