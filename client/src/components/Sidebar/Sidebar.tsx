82cee0a86c392070917b0ffbcd35c1a06a449799
Krishab Bashyal <krishab2002@gmail.com>
2024-10-05 13:36:58 -0500

    Changes app name

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
     <div className="w-60 min-h-screen bg-foreground flex flex-col flex-shrink-0">
       <div className="h-16 flex items-center justify-center gap-x-2 mt-2.5">
         <Image src="/images/logo.png" className="rounded-full" width={44} height={44} alt="Logo" />
-        <p className="text-2xl font-extrabold">AppTrack</p>
+        <p className="text-2xl font-extrabold">Apptive</p>
       </div>
       <div></div>
       <div className="flex flex-col mt-2.5 mx-4 flex-grow">
