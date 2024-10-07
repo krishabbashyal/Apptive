52d5b363f712382e051eaaa3fe44dd5a2646277e
Krishab Bashyal <krishab2002@gmail.com>
2024-10-07 16:03:25 -0500

    Setup server validation and logging for sihn up form

---
+++ client/src/app/(auth)/actions.ts
@@ -0,0 +1,13 @@
+'use server'
+
+import { signUpSchema, SignUpSchemaType } from "@/lib/schemas"
+
+
+export const logFormData = (data: SignUpSchemaType) => {
+  const validatedData = signUpSchema.safeParse(data)
+  if (validatedData.success) {
+    console.log("Valid data", data)
+  } else {
+    console.log("Invalid data", data)
+  }
+}
