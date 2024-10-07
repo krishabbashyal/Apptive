3d5e28f930a821a647d41819632d254bb35e2b57
Krishab Bashyal <krishab2002@gmail.com>
2024-10-07 16:10:54 -0500

    Added new action for logging in

--- client/src/app/(auth)/actions.ts
+++ client/src/app/(auth)/actions.ts
@@ -2,8 +2,19 @@
 
 import { signUpSchema, SignUpSchemaType } from "@/lib/schemas"
 
-export const logFormData = (data: SignUpSchemaType) => {
+export const signupUser = (data: SignUpSchemaType) => {
   const validatedData = signUpSchema.safeParse(data)
+  if (validatedData.success) {
+    console.log("Valid data", data)
+  } else {
+    console.log("Invalid data", data)
+  }
+}
+
+import { logInSchema, LogInSchemaType } from "@/lib/schemas"
+
+export const loginUser = (data: LogInSchemaType) => {
+  const validatedData = logInSchema.safeParse(data)
   if (validatedData.success) {
     console.log("Valid data", data)
   } else {
