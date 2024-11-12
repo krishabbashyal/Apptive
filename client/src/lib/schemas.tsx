70c73b6f60b2e7f66cf1d27ffaf0e71d53c87479
Krishab Bashyal <krishab2002@gmail.com>
2024-11-12 16:32:18 -0600

    Created a new schema and type for the application form

--- client/src/lib/schemas.tsx
+++ client/src/lib/schemas.tsx
@@ -1,4 +1,4 @@
-import { z} from "zod";
+import { z } from "zod";
 
 // Schema for signing up
 export const signUpSchema = z.object({
@@ -6,16 +6,13 @@ export const signUpSchema = z.object({
   lastName: z.string().min(2, "Must be at least 2 characters"),
   email: z.string().email("Please enter a valid email"),
   password: z.string().min(8, "Password must be at least 8 characters"),
-  termsAndConditions: z
-    .boolean()
-    .refine((val) => val === true, {
-      message: "You must agree to the terms and conditions",
-    }),
+  termsAndConditions: z.boolean().refine((val) => val === true, {
+    message: "You must agree to the terms and conditions",
+  }),
 });
 
 export type SignUpSchemaType = z.infer<typeof signUpSchema>;
 // end schema for signing up
-
 
 // Schema for logging in
 export const logInSchema = z.object({
@@ -23,6 +20,26 @@ export const logInSchema = z.object({
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
-export type LogInSchemaType = z.infer<typeof logInSchema>
+export type LogInSchemaType = z.infer<typeof logInSchema>;
 // end schema for logging in
 
+// Schema for creating a new application
+export const applicationSchema = z.object({
+  title: z.string().min(1, "Job title is required"),
+  company: z.string().min(1, "Company name is required"),
+  location: z.string().min(1, "Location is required"),
+  salary: z
+    .number({
+      invalid_type_error: "Salary must be a number",
+    })
+    .nonnegative({
+      message: "Salary must be a positive number",
+    }).optional(),
+  status: z.string().min(2, ""),
+  arrangement: z.string().min(1, "Select work arrangement"),
+  date: z.string().date("Must be a valid date"),
+  listingURL: z.string().url("Must be a valid URL").optional(),
+  notes: z.string().optional(),
+});
+
+export type ApplicationSchemaType = z.infer<typeof applicationSchema>;
