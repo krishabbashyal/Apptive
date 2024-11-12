d92a5df79b83c04cd05754cabf340a19f07c5979
Krishab Bashyal <krishab2002@gmail.com>
2024-11-12 14:37:05 -0600

    Small changes trying to get this layout to work

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -4,11 +4,12 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen w-full">
-      {/* Sidebar */}
-      <Sidebar />
-      <div className="flex w-full flex-col">
-        <div className="h-full w-full bg-background">
+    <div className="flex flex-row min-h-screen w-full">
+      <div className="">
+        <Sidebar />
+      </div>
+      <div className="flex w-full">
+        <div className="h-full w-full bg-red-200 flex items-center justify-center">
           {children}
         </div>
       </div>
