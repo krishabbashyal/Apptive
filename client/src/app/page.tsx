f15ba85ae40fccb5cd73a7b32b464b4deaa5c7cf
Krishab Bashyal <krishab2002@gmail.com>
2024-11-30 02:30:52 -0600

    Some landing page changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,32 +1,35 @@
-import React from 'react'
-
-
+import React from "react";
 
 const LandingPage = () => {
   return (
     <div>
-      <div className='flex flex-col md:flex-row h-[calc(100vh-6rem)] w-full bg-foreground'>
-        <div className='md:w-3/5'>
-          <div className='mx-4 md:mx-32'>
-            <div className='mt-10 md:mt-16'>
-              <h1 className='text-5xl md:text-6xl lg:text-8xl font-semibold'>Applications</h1>
-              <h1 className='text-5xl md:text-6xl lg:text-8xl font-semibold'>Managed</h1>
-              <h1 className='text-5xl md:text-6xl lg:text-8xl font-semibold'>Easily</h1>
+      <div className="flex h-[calc(100vh-6rem)] w-full flex-col bg-foreground md:flex-row">
+        <div className="md:w-3/5">
+          <div className="mx-4 md:mx-32">
+            <div className="mt-10 md:mt-16">
+              <h1 className="text-5xl font-semibold md:text-6xl lg:text-8xl">
+                Applications
+              </h1>
+              <h1 className="text-5xl font-semibold md:text-6xl lg:text-8xl">
+                Managed
+              </h1>
+              <h1 className="text-5xl font-semibold md:text-6xl lg:text-8xl">
+                Easily
+              </h1>
             </div>
 
             <div>
-              <p className='text-xl md:text-3xl font-medium mt-4 md:mt-10 text-spacer'>Elevate your job search with a tool that helps you stay organized and on top</p>
-            </div>
-            <div>
-            <button className='text-xl bg-white text-black font-semibold px-6 py-2.5 rounded-lg mt-4 md:mt-10'>Get started</button>
+              <p className="mt-4 text-3xl font-medium text-gray-300 md:mt-10 md:text-4xl">
+                Stay organized, efficient, and focused on your job search.
+              </p>
             </div>
           </div>
         </div>
-        <div className='md:w-2/5 mt-8 md:mt-0 bg-background'>
+        <div className="mt-8 bg-background md:mt-0 md:w-2/5">
           Right Side Content
         </div>
       </div>
     </div>
-  )
-}
-export default LandingPage
+  );
+};
+export default LandingPage;
--- client/src/components/AuthForms/AuthCard.tsx
+++ client/src/components/AuthForms/AuthCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AuthCard = () => {
   return (
-<div className='w-full h-full bg-gradient-to-tr from-accent to-accentLight rounded-xl overflow-hidden'>
+<div className='w-full h-full bg-gradient-to-tr from-accent to-accent rounded-xl overflow-hidden'>
 
 </div>
 
