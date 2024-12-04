a05ed04430b9a3ba5e349729593759e356198d86
Krishab Bashyal <krishab2002@gmail.com>
2024-12-04 17:54:03 -0600

    Updated colors in provioders

--- client/src/components/AuthForms/OAuthProvider.tsx
+++ client/src/components/AuthForms/OAuthProvider.tsx
@@ -10,7 +10,7 @@ interface OAuthProviderProps {
 
 const OAuthProvider = ({ icon, label }: OAuthProviderProps) => {
   return (
-    <div className='flex h-10 w-full items-center cursor-pointer border hover:ring-1 ring-accent border-cadetGray justify-center space-x-2 rounded' >
+    <div className='flex h-10 w-full items-center cursor-pointer border hover:ring-1 ring-accent border-spacer justify-center space-x-2 rounded' >
       <Image src={icon} width={28} height={28} alt="Logo"></Image>
       <p className='font-medium text-base'>{label}</p>
     </div>
