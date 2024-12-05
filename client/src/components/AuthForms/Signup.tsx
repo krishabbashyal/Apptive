e7b8772e522badf2ab12e7184ef04a60eef44dd9
Krishab Bashyal <krishab2002@gmail.com>
2024-12-04 18:14:32 -0600

    Updated the sign up form to use the custom form component

--- client/src/components/AuthForms/Signup.tsx
+++ client/src/components/AuthForms/Signup.tsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signUpSchema, SignUpSchemaType } from "@/lib/schemas";
 import { signupUser } from "@/app/(auth)/actions";
-
+import CustomInput from "../Inputs/CustomInput";
 
 const SignupForm = () => {
   const {
@@ -23,12 +23,8 @@ const SignupForm = () => {
     signupUser(data);
   };
 
-  const inputFieldClass = "h-10 w-full rounded border border-cadetGray bg-background pl-3 shadow placeholder:text-spacer focus:outline-none focus:ring-1 focus:ring-accent mt-0.5";
-  const inputFieldErrorClass = "h-10 w-full rounded border border-danger bg-background pl-3 shadow placeholder:text-spacer focus:outline-none focus:ring-1 focus:ring-accent animate-shake mt-0.5";
-  const errorMessageClass = "text-danger text-sm mt-1";
-
   return (
-    <div className="md:mx-4 flex w-full max-w-[60rem] flex-row justify-between  rounded-xl md:border border-graySeperator bg-foreground lg:rounded-r-xl">
+    <div className="flex w-full max-w-[60rem] flex-row justify-between rounded-xl border-spacer bg-foreground md:mx-4 md:border lg:rounded-r-xl">
       <div className="hidden p-2 md:block lg:w-7/12">
         <AuthCard />
       </div>
@@ -43,102 +39,53 @@ const SignupForm = () => {
           </h1>
           <p className="mt-2 text-sm sm:mt-4">
             Already have an account?{" "}
-            <span className="cursor-pointer text-accent"><Link href={"/login"}>Log in</Link></span>
+            <span className="cursor-pointer text-accent">
+              <Link href={"/login"}>Log in</Link>
+            </span>
           </p>
-          <div>
-            <div className="mt-4 flex flex-col gap-4 sm:flex-row">
-              <div className="flex w-full flex-col">
-                {/* First Name Input */}
-                <label htmlFor="firstName">First name</label>
-                <input
-                  {...register("firstName")}
-                  className={errors.firstName ? inputFieldErrorClass : inputFieldClass}
-                  type="text"
-                  id="firstName"
 
-                />
-                {errors.firstName && (
-                  <p className={errorMessageClass}>
-                    {errors.firstName.message}
-                  </p>
-                )}
-              </div>
-              <div className="flex w-full flex-col">
-                {/* Last Name Input */}
-                <label htmlFor="lastName">Last name</label>
-                <input
-                  {...register("lastName")}
-                  className={errors.lastName ? inputFieldErrorClass : inputFieldClass}
-                  type="text"
-                  id="lastName"
-                />
-                {errors.lastName && (
-                  <p className={errorMessageClass}>
-                    {errors.lastName.message}
-                  </p>
-                )}
-              </div>
-            </div>
-            <div className="mt-2 flex flex-col gap-y-2">
-              <div>
-                {/* Email Input */}
-                <label htmlFor="email">Email</label>
-                <input
-                  {...register("email")}
-                  className={errors.email ? inputFieldErrorClass : inputFieldClass}
-                  type="email"
-
-                  id="email"
-                />
-                {errors.email && (
-                  <p className={`${errorMessageClass}`}>
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-              <div>
-                {/* Password Input */}
-                <label htmlFor="password">Password</label>
-                <input
-                  {...register("password")}
-                  type="password"
-                  className={errors.password ? inputFieldErrorClass : inputFieldClass}
-    
-                  id="password"
-                />
-                {errors.password && (
-                  <p className={errorMessageClass}>
-                    {errors.password.message}
-                  </p>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="mt-4 flex flex-col">
-            {/* Terms and Conditions Checkbox */}
-            <label htmlFor="termsAndConditions">Terms and Conditions</label>
-            <div className="flex flex-row gap-x-2 mt-0.5 items-center">
-              <input
-                {...register("termsAndConditions")}
-                className={`h-5 w-5 accent-accent rounded-md cursor-pointer ${errors.termsAndConditions ? "animate-shake" : ""}`}
-                type="checkbox"
-                id="termsAndConditions"
+          <div className="mt-6 sm:flex-row">
+            <div className="flex w-full flex-row gap-x-4">
+              <CustomInput
+                label="First name"
+                id="firstName"
+                type="text"
+                register={register("firstName")}
+                error={errors.firstName}
               />
-              <p className={ errors.termsAndConditions ? "animate-shake text-danger" : "text-spacer"}>
-                I agree to the{" "}
-                <span className="cursor-pointer text-accent">
-                  Terms and Conditions
-                </span>
-              </p>
+              <CustomInput
+                label="Last name"
+                id="lastName"
+                type="text"
+                register={register("lastName")}
+                error={errors.lastName}
+              />
+            </div>
+            <div className="mt-5 flex flex-col gap-y-5">
+              <CustomInput
+                label="Email address"
+                id="email"
+                type="email"
+                register={register("email")}
+                error={errors.email}
+              />
+              <CustomInput
+                label="Password"
+                id="password"
+                type="password"
+                register={register("password")}
+                error={errors.password}
+              />
             </div>
           </div>
+
           <button
-            className="mt-6 h-12 rounded-md from-accent to-accent px-4 py-2 font-medium text-white enabled:bg-gradient-to-tr disabled:cursor-not-allowed disabled:bg-cadetGray sm:mt-8"
+            className="mt-6 h-12 rounded-md from-accent to-accent px-4 py-2 font-medium text-white enabled:bg-gradient-to-tr disabled:cursor-not-allowed disabled:bg-spacer sm:mt-8"
             disabled={isSubmitting}
           >
             Create account
           </button>
-          <div className="relative mt-5 border-b border-cadetGray text-center text-sm">
+          <div className="relative mt-5 border-b border-spacer text-center text-sm">
             <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform bg-foreground px-3 text-white">
               Or register with
             </span>
