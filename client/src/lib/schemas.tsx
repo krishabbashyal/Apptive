e2676e902eff95f276a27ba1c99d806dedfffe76
Krishab Bashyal <krishab2002@gmail.com>
2024-12-04 18:13:28 -0600

    Removed the need for terms and conditions

--- client/src/lib/schemas.tsx
+++ client/src/lib/schemas.tsx
@@ -6,9 +6,6 @@ export const signUpSchema = z.object({
   lastName: z.string().min(2, "Must be at least 2 characters"),
   email: z.string().email("Please enter a valid email"),
   password: z.string().min(8, "Password must be at least 8 characters"),
-  termsAndConditions: z.boolean().refine((val) => val === true, {
-    message: "You must agree to the terms and conditions",
-  }),
 });
 
 export type SignUpSchemaType = z.infer<typeof signUpSchema>;
