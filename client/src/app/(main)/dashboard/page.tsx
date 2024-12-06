145e0a78368d315244fd3ec2d1f4545396bcbad8
Krishab Bashyal <krishab2002@gmail.com>
2024-12-06 14:12:13 -0600

    Started working on the statcards on the dashboard page

--- client/src/app/(main)/dashboard/page.tsx
+++ client/src/app/(main)/dashboard/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 import ApplicationForm from '@/components/Forms/ApplicationForm'
+import StatCard from '@/components/UserInterface/StatCard'
 
 const Dashboard = () => {
   return (
     <div className='w-full h-full'>
-      <ApplicationForm />
+      {/* <ApplicationForm /> */}
+      <div className='mx-16'>
+        <div className='flex flex-row justify-between mt-8 gap-x-8'>
+            <StatCard stat='12' description='Applications sent' />
+            <StatCard stat='5' description='Awaiting response' />
+            <StatCard stat='5' description='In progress' />
+            <StatCard stat='5' description='Archived' />
+            <StatCard stat='2' description='Offers received' />
+            <StatCard stat='12' description='Response rate' />
+        </div>
+      </div>
     </div>
   )
 }
