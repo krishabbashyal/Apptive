1accf2559a98287ae47abaf8463c27c4c579096e
Krishab Bashyal <krishab2002@gmail.com>
2024-12-10 15:32:37 -0600

    Added the application form to the dashboard screen

--- client/src/app/(main)/dashboard/page.tsx
+++ client/src/app/(main)/dashboard/page.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import ApplicationForm from '@/components/Forms/ApplicationForm'
-import StatCard from '@/components/UserInterface/StatCard'
-import RecentActivity from '@/components/UserInterface/RecentActivity'
+import ApplicationForm from "@/components/Forms/ApplicationForm";
+import StatCard from "@/components/UserInterface/StatCard";
+import RecentActivity from "@/components/UserInterface/RecentActivity";
 
 const Dashboard = () => {
   return (
-    <div className='w-full h-full'>
-      {/* <ApplicationForm /> */}
-      <div className='mx-16'>
-        <div className='flex flex-row justify-between mt-8 gap-x-8'>
-            <StatCard stat='12' description='Applications sent' />
-            <StatCard stat='5' description='Awaiting response' />
-            <StatCard stat='5' description='In progress' />
-            <StatCard stat='5' description='Archived' />
-            <StatCard stat='2' description='Offers received' />
-            <StatCard stat='12' description='Response rate' />
+    <div className="h-full w-full">
+
+        {/* <ApplicationForm /> */}
+
+
+      <div className="mx-16">
+        <div className="flex flex-row justify-between gap-x-8 pt-8">
+          <StatCard stat="12" description="Applications sent" />
+          <StatCard stat="5" description="Awaiting response" />
+          <StatCard stat="5" description="In progress" />
+          <StatCard stat="5" description="Archived" />
+          <StatCard stat="2" description="Offers received" />
+          <StatCard stat="12" description="Response rate" />
         </div>
 
-        <div className='mt-8'>
+        <div className="mt-8">
           <RecentActivity />
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
