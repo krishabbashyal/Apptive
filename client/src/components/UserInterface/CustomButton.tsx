748f29449bd9667fe71b5cf0967dd81e32831b52
Krishab Bashyal <krishab2002@gmail.com>
2024-12-10 16:51:30 -0600

    Created a custom button component

--- client/src/components/UserInterface/CustomButton.tsx
+++ client/src/components/UserInterface/CustomButton.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { IconProps } from '@phosphor-icons/react'
+import React from 'react' 
+import { IconProps } from '@phosphor-icons/react'  
 
-interface CustomButtonProps {
-  label: string
-  icon?: React.ComponentType<IconProps>
-}
+interface CustomButtonProps {   
+  label: string   
+  onClick?: () => void   
+  icon?: React.ComponentType<IconProps> 
+}  
 
-const CustomButton = ({ label, icon: Icon }: CustomButtonProps) => {
-  return (
-    <button className='h-10 px-4 bg-accent rounded-lg text-white'>
-    <div className='flex flex-row gap-x-2'>
-      {Icon && <Icon size={24} />}
-      <p className='font-medium'>{label}</p>
-    </div>
-  </button>
-  )
-}
+const CustomButton = ({ 
+  label, 
+  icon: Icon, 
+  onClick 
+}: CustomButtonProps) => {   
+  return (     
+    <button 
+      className='h-10 px-4 bg-accent rounded-lg text-white'
+      onClick={onClick}
+    >     
+      <div className='flex flex-row items-center gap-x-2'>       
+        {Icon && <Icon size={24} />}       
+        <p className='font-medium'>{label}</p>     
+      </div>   
+    </button>   
+  ) 
+}  
 
 export default CustomButton
