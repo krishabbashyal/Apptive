a2fd63617e6561bfb86317f6eca235679841f191
Krishab Bashyal <krishab2002@gmail.com>
2024-12-18 15:35:22 -0600

    Finished prisma setup

--- client/prisma/migrations/20241218193717_db_fixes/migration.sql
+++ client/prisma/migrations/20241218193717_db_fixes/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "profilePicture" TEXT,
+    "passwordHash" TEXT,
+    "profileImageUrl" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "googleId" TEXT,
     "githubId" TEXT,
@@ -16,36 +16,53 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Application" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "applicationDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "title" TEXT NOT NULL,
     "company" TEXT NOT NULL,
-    "location" TEXT,
+    "locationId" TEXT NOT NULL,
     "salary" INTEGER,
     "status" TEXT NOT NULL,
-    "style" TEXT,
-    "archived" BOOLEAN NOT NULL DEFAULT false,
+    "workStyle" TEXT,
+    "isArchived" BOOLEAN NOT NULL DEFAULT false,
     "applicationMethod" TEXT,
     "referral" TEXT,
     "notes" TEXT,
-    "resume" TEXT,
+    "resumeUrl" TEXT,
     "applicationUrl" TEXT,
-    "applicationBoardId" INTEGER,
+    "applicationBoardId" TEXT,
 
     CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "ApplicationBoard" (
-    "id" SERIAL NOT NULL,
-    "boardTitle" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "ApplicationBoard_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Location" (
+    "id" TEXT NOT NULL,
+    "stateCode" TEXT NOT NULL,
+    "stateName" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "county" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "queryCount" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Application" ADD CONSTRAINT "Application_applicationBoardId_fkey" FOREIGN KEY ("applicationBoardId") REFERENCES "ApplicationBoard"("id") ON DELETE SET NULL ON UPDATE CASCADE;
