c59d04f255a4ae9d275ee1d3e6953e807db6b51b
Krishab Bashyal <krishab2002@gmail.com>
2024-12-20 11:27:13 -0600

    Editied the landing page to use the custom components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,34 +1,14 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-import { ArrowRight, GithubLogo} from "@phosphor-icons/react";
+import Navbar from "@/components/UserInterface/Navbar";
+import { ArrowRight, GithubLogo } from "@phosphor-icons/react";
+import CustomButton from "@/components/UserInterface/CustomButton";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen bg-background text-white">
-      {/* Navigation */}
-      <nav className="px-16 flex items-center justify-between bg-foreground py-6">
-        <div className="flex items-center gap-x-2">
-          <Image
-            src="/images/logo.png"
-            className="rounded-full"
-            width={38}
-            height={38}
-            alt="Logo"
-          />
-          <p className="text-2xl font-extrabold">Apptive</p>
-        </div>
-        <div className="flex items-center gap-6">
-          <a href="#" className="transition-colors hover:text-accent">
-            Log in
-          </a>
-          <button className="rounded-lg bg-accent px-4 py-2 transition-colors">
-            Sign Up
-          </button>
-        </div>
-      </nav>
-
+    <div className="min-h-screen text-white">
+      <Navbar />
       <div className="px-16">
         <div className="mt-10 md:mt-16">
           <h1 className="text-5xl font-semibold md:text-6xl lg:text-8xl">
@@ -46,16 +26,24 @@ const LandingPage = () => {
             Stay organized, efficient, and focused on your job search.
           </p>
           <div className="mt-12 flex flex-col gap-6 sm:flex-row">
-            <button className="flex items-center justify-center gap-2 rounded-lg bg-accent px-6 py-3 text-lg transition-colors hover:bg-opacity-80">
-              Get Started <ArrowRight size={20} weight="bold" />
-            </button>
-            <button className="flex items-center justify-center gap-2 rounded-lg border border-spacer bg-foreground px-6 py-3 text-lg transition-colors hover:bg-accentHighlight">
-              <GithubLogo size={20} weight="bold" /> Star on GitHub
-            </button>
+            <CustomButton
+              label="Get Started"
+              customClasses="bg-accent w-48 h-14"
+              icon={ArrowRight}
+              iconSide="right"
+              textClasses="text-lg"
+              onClick={() => {}}
+            />
+            <CustomButton
+              label="Star on Github"
+              customClasses="bg-foreground w-56 hover:bg-opacity-100 hover:bg-accentHighlight border border-spacer h-14"
+              icon={GithubLogo}
+              textClasses="text-lg"
+              onClick={() => {}}
+            />
           </div>
-          
         </div>
-      </div>    
+      </div>
     </div>
   );
 };
