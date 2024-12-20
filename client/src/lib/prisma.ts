57b7e6649eac41ff87e3cf68c6c00a32e88bd529
Krishab Bashyal <krishab2002@gmail.com>
2024-12-20 01:51:08 -0600

    More prisma changes to get it working in prod

--- client/src/lib/prisma.ts
+++ client/src/lib/prisma.ts
@@ -1,11 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// Ensure TypeScript type safety for the global object
+const globalForPrisma = global as typeof global & {
+  prisma?: PrismaClient;
+};
 
+// Initialize Prisma client
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma
+// Assign Prisma to the global object in development
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
