790f7b2b6b1608a69a3b06e0b2ba7690ab4a3ecf
Krishab Bashyal <krishab2002@gmail.com>
2024-12-20 11:27:44 -0600

    Added more customization options for the custom buttons

--- client/src/components/UserInterface/CustomButton.tsx
+++ client/src/components/UserInterface/CustomButton.tsx
@@ -3,23 +3,29 @@ import { IconProps } from '@phosphor-icons/react'
 
 interface CustomButtonProps {   
   label: string   
+  customClasses?: string
   onClick?: () => void   
   icon?: React.ComponentType<IconProps> 
+  iconSide?: 'left' | 'right'
+  textClasses?: string
 }  
 
 const CustomButton = ({ 
   label, 
+  customClasses,
+  textClasses,
   icon: Icon, 
+  iconSide = 'left',
   onClick 
 }: CustomButtonProps) => {   
   return (     
     <button 
-      className='h-10 px-4 bg-accent rounded-lg text-white'
+      className={`h-10 px-4 rounded-lg hover:bg-opacity-80 text-white ${customClasses}`}
       onClick={onClick}
     >     
-      <div className='flex flex-row items-center gap-x-2'>       
+      <div className={`flex flex-row items-center justify-center gap-x-2 ${iconSide === 'right' && 'flex-row-reverse'}`}>       
         {Icon && <Icon size={24} />}       
-        <p className='font-medium'>{label}</p>     
+        <p className={`font-medium ${textClasses}`}>{label}</p>     
       </div>   
     </button>   
   ) 
