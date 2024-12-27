9748d9b41e2ea667f2315e098ef55dfb043e441c
Krishab Bashyal <krishab2002@gmail.com>
2024-12-27 17:31:56 -0600

    Navbar responsive changes for mobile

--- client/src/components/UserInterface/Navbar.tsx
+++ client/src/components/UserInterface/Navbar.tsx
@@ -5,7 +5,7 @@ import CustomButton from "./CustomButton";
 const Navbar = () => {
   return (
     <div>
-      <nav className="flex items-center justify-between bg-none px-16 py-5">
+      <nav className="flex items-center justify-between bg-none px-6 lg:px-16 py-5">
         <div className="flex items-center gap-x-2">
           <Image
             src="/images/logo.png"
@@ -16,7 +16,7 @@ const Navbar = () => {
           />
           <p className="text-2xl font-extrabold">Apptive</p>
         </div>
-        <div className="flex items-center gap-6">
+        <div className="items-center hidden lg:flex gap-6">
           <CustomButton label="Log In" textClasses="hover:text-accent" customClasses="bg-foreground"/>
           <CustomButton label="Sign up" customClasses="bg-accent"/>
         </div>
