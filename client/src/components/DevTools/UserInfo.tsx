f88b9c468f1d9c711ce7a7669feef2642c5bf104
Krishab Bashyal <krishab2002@gmail.com>
2025-01-15 00:13:52 -0600

    Updated UserInfo component to show the currently logged in user

--- client/src/components/DevTools/UserInfo.tsx
+++ client/src/components/DevTools/UserInfo.tsx
@@ -3,7 +3,6 @@ import { User } from "@supabase/supabase-js";
 import { getUser } from "@/app/(auth)/actions";
 import { useState, useEffect } from "react";
 
-
 const UserInfo = () => {
   const [user, setUser] = useState<User | null>(null);
 
@@ -20,7 +19,26 @@ const UserInfo = () => {
     fetchUser();
   }, []);
 
-  return <div className="absolute opacity-30 text-accent">{JSON.stringify(user, null, 2)}</div>;
+  return (
+    <div className="fixed bottom-4 right-[210px] py-[18px] rounded-lg border border-accent bg-accentHighlight p-4 shadow-lg backdrop-blur-sm">
+      <h1 className="mb-2 text-slate-200">Logged-In User:</h1>
+
+      <div className="space-y-2 font-mono text-sm">
+        <p className="flex justify-between">
+          <span className="text-slate-400">Status:</span>
+          <span className="text-emerald-400">{user?.role || "N/A"}</span>
+        </p>
+        <p className="flex justify-between">
+          <span className="text-slate-400">Email:</span>
+          <span className="text-emerald-400">{user?.email || "N/A"}</span>
+        </p>
+        <p className="flex justify-between">
+          <span className="text-slate-400 mr-4">ID:</span>
+          <span className="text-emerald-400">{user?.id || "N/A"}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default UserInfo;
