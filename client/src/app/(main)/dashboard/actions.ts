5a7e8ca6845a02b186296698552de641aad7f4c6
Krishab Bashyal <krishab2002@gmail.com>
2025-01-15 16:56:49 -0600

    Started working on data mutation to submit the application form

--- client/src/app/(main)/dashboard/actions.ts
+++ client/src/app/(main)/dashboard/actions.ts
@@ -1,12 +1,39 @@
 'use server'
 
-import { applicationSchema, ApplicationSchemaType } from "@/lib/schemas"
+import { applicationSchema } from "@/lib/schemas"
 import { prisma } from "@/lib/prisma"
+import { getUser } from "@/app/(auth)/actions"
 
-export const createApplication = async (data: ApplicationSchemaType) => {
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+const toLocalISODateTime = (dateStr: string): Date => {
+  // This preserves local timezone
+  return new Date(`${dateStr}T00:00:00`);
+}
+
+export const createApplication = async (data: any) => {
   const validatedData = applicationSchema.safeParse(data)
   if (validatedData.success) {
     console.log("Valid data", data)
+  
+    const res = await getUser()
+    const userId = res?.id
+
+    await prisma.application.create({ 
+      data: {
+        title: data.title,
+        company: data.company,
+        locationId: data.location,
+        salary: data.salary,
+        status: data.status,
+        workStyle: data.arrangement,
+        applicationDate: toLocalISODateTime(data.date),
+        applicationUrl: data.listingURL,
+        notes: data.notes,
+        userID: userId
+      }
+     })
   } else {
     console.log("Invalid data", data)
   }
