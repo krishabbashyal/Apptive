f234f8105a455b8bb577cd7e76390b2266a958af
Krishab Bashyal <krishab2002@gmail.com>
2025-01-24 18:40:40 -0600

    Updated the archive modal to show more data

--- client/src/lib/store/useShowArchiveModal.ts
+++ client/src/lib/store/useShowArchiveModal.ts
@@ -5,19 +5,23 @@ import { create } from "zustand";
 
 type ArchiveModalState = {
   archiveModalShown: boolean;
-  archiveModalData: string | null;
+  id: string | null;
+  title: string | null;
+  company: string | null
 };
 
 type ArchiveModalAction = {
-  openArchiveModal: (data: string) => void;
+  openArchiveModal: (id: string, title: string, company: string) => void;
   closeArchiveModal: () => void;
 };
 
 const useShowArchiveModal = create<ArchiveModalState & ArchiveModalAction>((set) => ({
   archiveModalShown: false,
-  archiveModalData: null,
-  openArchiveModal: (data: string) => set({ archiveModalShown: true, archiveModalData: data }),
-  closeArchiveModal: () => set({ archiveModalShown: false, archiveModalData: null }),
+  id: null,
+  title: null,
+  company: null,
+  openArchiveModal: (id: string, title: string, company: string) => set({ archiveModalShown: true, id: id, title: title, company: company }),
+  closeArchiveModal: () => set({ archiveModalShown: false, id: null, title: null, company: null }),
 }));
 
 export default useShowArchiveModal;
