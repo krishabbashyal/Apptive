fc91fe8d66e115b0073cec6f4df3e0578536d232
Krishab Bashyal <krishab2002@gmail.com>
2025-01-25 17:44:16 -0600

    Created a new dropdown component to house the application actions like edit, link and archive

--- client/src/components/UserInterface/ApplicationRowActions.tsx
+++ client/src/components/UserInterface/ApplicationRowActions.tsx
@@ -1,100 +1,127 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Application } from "@prisma/client";
-import { 
-  DotsThreeVertical, 
-  PencilSimple, 
-  Link as LinkIcon, 
-  Archive, 
+import {
+  DotsThreeVertical,
+  PencilSimple,
+  Link as LinkIcon,
+  Archive,
 } from "@phosphor-icons/react";
 
-interface ActionButtonProps {
-  label: string;
-  icon: React.ReactNode;
-  onClick: () => void;
+import { archiveApplication } from "@/app/(main)/dashboard/actions";
+
+
+interface ButtonProps {
+  application: Application;
+  setIsOpen: (isOpen: boolean) => void;
 }
 
-const ActionButton: React.FC<ActionButtonProps> = ({ label, icon, onClick }) => (
-  <button
-    onClick={onClick}
-    className="px-4 flex items-center gap-x-4 py-2.5 text-left border-b last:border-0 border-spacer/20 hover:bg-accent/10 transition-colors"
-  >
-    {icon}
-    {label}
-  </button>
-);
+const EditButton = ({ application }: ButtonProps) => {
+  return (
+    <button
+      onClick={() => {}}
+      className="flex items-center gap-x-4 border-b border-spacer/30 px-4 py-2.5 text-left transition-colors last:border-0 hover:text-yellow-500"
+    >
+      <PencilSimple size={20} />
+      <p>Edit</p>
+    </button>
+  );
+};
+
+const LinkButton = ({ application }: ButtonProps) => {
+  return (
+    <button
+      onClick={() => {}}
+      className="flex items-center gap-x-4 border-b border-spacer/30 px-4 py-2.5 text-left transition-colors last:border-0 hover:text-blue-500"
+    >
+      <LinkIcon size={20} />
+      <p>Application link</p>
+    </button>
+  );
+};
+
+const ArchiveButton = ({ application, setIsOpen }: ButtonProps) => {
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const handleArchiveApplication = async (id: string | null) => {
+    if (!id) return;
+    await archiveApplication(id);
+    setShowConfirmation(false);
+    setIsOpen(false);
+
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => setShowConfirmation(true)}
+        className="flex items-center gap-x-4 border-spacer/30 px-4 py-2.5 text-left transition-colors hover:text-danger"
+      >
+        <Archive className={showConfirmation ? "text-danger" : ""} size={20} />
+        <p className={showConfirmation ? "text-danger" : ""}>
+          {showConfirmation ? "Confirm archive?" : "Archive"}
+        </p>
+      </button>
+
+      {showConfirmation && (
+        <div className="mt-1 flex flex-row justify-between gap-x-2 px-4 pb-2">
+          <button
+            className="rounded-lg border border-spacer/60 p-1.5 px-3 font-extralight hover:bg-spacer/10"
+            onClick={() => setShowConfirmation(false)}
+          >
+            Cancel
+          </button>
+          <button
+            className="rounded-lg border border-danger/25 p-1.5 px-3 text-danger hover:bg-danger/10"
+            onClick={() => handleArchiveApplication(application.id)}
+          >
+            Archive
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 interface ApplicationRowActionsProps {
   application: Application;
 }
 
-const ApplicationRowActions: React.FC<ApplicationRowActionsProps> = ({ application }) => {
+const ApplicationRowActions = ({ application }: ApplicationRowActionsProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
-        dropdownRef.current && 
+        dropdownRef.current &&
         !dropdownRef.current.contains(event.target as Node)
       ) {
         setIsOpen(false);
-
       }
     };
-
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
-
-  const handleArchive = () => {
-  };
-
-  const actionButtons = [
-    { 
-      label: "Edit", 
-      icon: <PencilSimple size={20} />,
-      onClick: () => {} 
-    },
-    { 
-      label: "Application URL", 
-      icon: <LinkIcon size={20} />,
-      onClick: () => {} 
-    },
-    { 
-      label: "Archive", 
-      icon: <Archive size={20} />,
-      onClick: handleArchive 
-    }
-  ];
 
   return (
     <div className="relative" ref={dropdownRef}>
       <button
         type="button"
         onClick={() => setIsOpen(!isOpen)}
-        className="rounded-lg border border-spacer/20 bg-spacer/20 p-0.5 mt-1.5 text-gray-400 transition-all duration-200 ease-in-out hover:scale-110 hover:border-accent hover:bg-spacer/50 hover:text-gray-200"
+        className={`mt-1.5 rounded-lg border bg-spacer/20 p-0.5 text-gray-400 transition-all duration-200 ease-in-out hover:scale-110 hover:border-accent hover:bg-spacer/50 ${isOpen ? "border-accent" : "border-spacer/20"} hover:text-gray-200`}
       >
         <DotsThreeVertical size={24} />
       </button>
-
       {isOpen && (
-        <div className="absolute right-0 border border-spacer z-10 mt-2 w-48 rounded-lg bg-foreground py-1 shadow-lg">
+        <div className="absolute right-0 z-10 mt-2 w-52 rounded-lg border border-spacer bg-foreground py-1 shadow-lg">
           <div className="flex flex-col">
-            {actionButtons.map((action) => (
-              <ActionButton
-                key={action.label}
-                label={action.label}
-                icon={action.icon}
-                onClick={action.onClick}
-                />
-              ))}
+            <EditButton application={application} setIsOpen={setIsOpen} />
+            <LinkButton application={application} setIsOpen={setIsOpen} />
+            <ArchiveButton application={application} setIsOpen={setIsOpen} />
           </div>
         </div>
       )}
-    
-
     </div>
   );
 };
