1020458bfd514363a8b4015fa6f65401d16d62ad
Krishab Bashyal <krishab2002@gmail.com>
2025-01-27 20:27:19 -0600

    Removed dev buttons

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import BreakpointInfo from "@/components/DevTools/BreakpointInfo";
-import UserInfo from "@/components/DevTools/UserInfo";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className="min-h-screen bg-background text-white">
-        <UserInfo />
-        <BreakpointInfo />
+
         {children}
       </body>
     </html>
--- client/src/components/UserInterface/ApplicationTable.tsx
+++ client/src/components/UserInterface/ApplicationTable.tsx
@@ -9,64 +9,67 @@ interface ApplicationTableProps {
 
 function ApplicationTable({ applicationData }: ApplicationTableProps) {
   return (
-    <div className="overflow-hidden rounded-xl bg-background">
-      <table className="w-full h-full">
-        <thead>
-          <tr className="border-b border-[#242424]">
-            <th className="px-6 py-4 text-left">
-              <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
-                Company & Role
-              </span>
-            </th>
-            <th className="px-6 py-4 text-left">
-              <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
-                Location
-              </span>
-            </th>
-            <th className="px-6 py-4 text-left">
-              <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
-                Application Date
-              </span>
-            </th>
-            <th className="px-6 py-4 text-left">
-              <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
-                Salary
-              </span>
-            </th>
-            <th className="px-6 py-4 text-left">
-              <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
-                Status
-              </span>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-[#242424]">
-          {applicationData.length > 0 ? (
-            applicationData.map((application: Application) => (
-              <ApplicationTableRow
-                key={application.id}
-                application={application}
-              />
-            ))
-          ) : (
-            <tr>
-              <td colSpan={6} className="px-6 py-12 h-96">
-                <div className="flex flex-col items-center justify-center text-center">
-                  <div className="rounded-full bg-accentHighlight/60 p-3 mb-4">
-                    <FolderOpen size={24} className="text-accent" />
-                  </div>
-                  <p className="text-base font-medium text-gray-300">
-                    No applications yet
-                  </p>
-                  <p className="mt-1 text-sm text-gray-300">
-                    Start tracking your job search by adding your first application
-                  </p>
-                </div>
-              </td>
+    <div className=" rounded-xl bg-background">
+      <div className="overflow-y-auto max-h-[calc(100vh-22rem)]">
+        <table className="w-full">
+          <thead>
+            <tr className="border-b border-[#242424]">
+              <th className="px-6 py-4 text-left">
+                <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
+                  Company & Role
+                </span>
+              </th>
+              <th className="px-6 py-4 text-left">
+                <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
+                  Location
+                </span>
+              </th>
+              <th className="px-6 py-4 text-left">
+                <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
+                  Application Date
+                </span>
+              </th>
+              <th className="px-6 py-4 text-left">
+                <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
+                  Salary
+                </span>
+              </th>
+              <th className="px-6 py-4 text-left">
+                <span className="text-sm font-medium uppercase tracking-wider text-gray-300">
+                  Status
+                </span>
+              </th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y divide-[#242424] pb-96">
+            {applicationData.length > 0 ? (
+              applicationData.map((application: Application) => (
+                <ApplicationTableRow
+                  key={application.id}
+                  application={application}
+                />
+              ))
+            ) : (
+              <tr>
+                <td colSpan={6} className="px-6 py-12 h-96">
+                  <div className="flex flex-col items-center justify-center text-center">
+                    <div className="rounded-full bg-accentHighlight/60 p-3 mb-4">
+                      <FolderOpen size={24} className="text-accent" />
+                    </div>
+                    <p className="text-base font-medium text-gray-300">
+                      No applications yet
+                    </p>
+                    <p className="mt-1 text-sm text-gray-300">
+                      Start tracking your job search by adding your first
+                      application
+                    </p>
+                  </div>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
