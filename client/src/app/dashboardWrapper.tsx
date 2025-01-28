a8eb75671cf6e96eddd5030a90e4d80150d9c2eb
Krishab Bashyal <krishab2002@gmail.com>
2025-01-27 19:45:38 -0600

    more sidebar fixes

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -5,10 +5,10 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-row min-h-screen">
-      <div>
+      <div className="flex flex-col w-[15rem] h-screen fixed bg-foreground">
         <Sidebar />
       </div>
-      <div className="flex w-full">
+      <div className="flex w-full pl-[15rem]">
         <div className="h-full w-full bg-background">
           {children}
         </div>
