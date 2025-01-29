3c5b2a27e854e74ab8bed64a706d663cb8ab3027
Krishab Bashyal <krishab2002@gmail.com>
2025-01-28 20:48:39 -0600

    Added total interview, added action call to gather job hunt data

--- client/src/components/UserInterface/StatCardContainer.tsx
+++ client/src/components/UserInterface/StatCardContainer.tsx
@@ -1,11 +1,9 @@
-'use server'
+"use server";
 
-import React from 'react'
-import StatCard from './StatCard'
+import React from "react";
+import StatCard from "./StatCard";
 import { fetchAllActiveApplications } from "@/app/(main)/dashboard/actions";
-import { fetchAllArchivedApplications } from '@/app/(main)/dashboard/actions';
-
-
+import { fetchAllArchivedApplications } from "@/app/(main)/dashboard/actions";
 
 const StatCardContainer = async () => {
   const activeApplications = await fetchAllActiveApplications();
@@ -14,31 +12,41 @@ const StatCardContainer = async () => {
   const allApplications = [...activeApplications, ...archivedApplications];
 
   const awaitingResponse = activeApplications.filter(
-    (application) => application.application_status === "Applied"
+    (application) => application.application_status === "Applied",
   ).length;
 
   const inProgress = activeApplications.filter(
-    (application) => application.application_status === "Interviewing"
-  ).length
+    (application) => application.application_status === "Interviewing",
+  ).length;
 
-  const rejected = activeApplications.filter(
-    (application) => application.application_status === "Rejected"
-  ).length + archivedApplications.length
+  const rejected = allApplications.filter(
+    (application) => application.application_status === "Rejected",
+  ).length;
 
   const offers = activeApplications.filter(
-    (application) => application.application_status === "Accepted"
-  ).length
+    (application) => application.application_status === "Accepted",
+  ).length;
+
+  const responseRate =
+    (allApplications.filter((application) => application.had_response).length /
+      allApplications.length) *
+    100;
+
+  const totalInterviews = activeApplications.filter(
+    (application) => application.had_response,
+  ).length;
 
   return (
     <div className="flex flex-row justify-between gap-x-8 pt-8">
-    <StatCard stat={allApplications.length} description="Applications sent" />
-    <StatCard stat={awaitingResponse} description="Awaiting response" />
-    <StatCard stat={inProgress} description="In progress" />
-    <StatCard stat={rejected} description="Rejected" />
-    <StatCard stat={offers} description="Offers received" />
-    <StatCard stat="12" description="Response rate" />
-  </div>
-  )
-}
+      <StatCard stat={allApplications.length} description="Applications sent" />
+      <StatCard stat={awaitingResponse} description="Awaiting response" />
+      <StatCard stat={inProgress} description="Interviewing" />
+      <StatCard stat={rejected} description="Rejected" />
+      <StatCard stat={totalInterviews} description="Total interviews" />
+      <StatCard stat={offers} description="Offers received" />
+      <StatCard stat={responseRate} description="Response rate" />
+    </div>
+  );
+};
 
-export default StatCardContainer
+export default StatCardContainer;
