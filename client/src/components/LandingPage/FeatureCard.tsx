0fa0e1ab431aef5a8ab4d856ce501204a3bea946
Krishab Bashyal <krishab2002@gmail.com>
2025-02-16 02:12:53 -0600

    Made changes to the feature card component

--- client/src/components/LandingPage/FeatureCard.tsx
+++ client/src/components/LandingPage/FeatureCard.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { IconProps } from '@phosphor-icons/react'
 
 interface FeatureCardProps {
+  icon?: React.ComponentType<IconProps>,
   title: string,
+  customClasses?: string
   description: string,
 }
-const FeatureCard = ({title, description}: FeatureCardProps) => {
+
+const FeatureCard = ({ title, icon: Icon, description, customClasses }: FeatureCardProps) => {
   return (
-    <div className='w-80 h-96 bg-background rounded-xl p-6 text-spacer ring-4 ring-spacer hover:ring-accent '>
-      <p className='text-2xl text-center pb-2 font-semibold border-b border-spacer '>{title}</p>
-      <p className='font-medium mt-4'>{description}</p>
+    <div className={`${customClasses} opacity-0`}>
+      <div className="w-full bg-foreground h-52 border border-accent/10 rounded-xl px-6 pt-6 transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-accentHighlight">
+        <div className='flex flex-row gap-x-4 items-center border-b border-spacer/40 pb-2.5'>
+          {Icon && <Icon className='text-accent bg-accent/10 rounded-full p-2.5 transition duration-200 hover:bg-accent/20 hover:scale-110' size={48} />}
+          <p className='text-2xl font-semibold'>{title}</p>
+        </div>
+        <p className='text-lg mt-3'>{description}</p>
+      </div>
     </div>
   )
 }
