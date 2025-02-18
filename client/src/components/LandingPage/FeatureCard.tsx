68e27176ff50e48d27d6068a4b64ae43a9b588d3
Krishab Bashyal <krishab2002@gmail.com>
2025-02-17 22:12:06 -0600

    Made some small UI tweaks to the feature card

--- client/src/components/LandingPage/FeatureCard.tsx
+++ client/src/components/LandingPage/FeatureCard.tsx
@@ -11,12 +11,12 @@ interface FeatureCardProps {
 const FeatureCard = ({ title, icon: Icon, description, customClasses }: FeatureCardProps) => {
   return (
     <div className={`${customClasses} opacity-0`}>
-      <div className="w-full bg-foreground h-52 border border-accent/10 rounded-xl px-6 pt-6 transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-accentHighlight">
-        <div className='flex flex-row gap-x-4 items-center border-b border-spacer/40 pb-2.5'>
-          {Icon && <Icon className='text-accent bg-accent/10 rounded-full p-2.5 transition duration-200 hover:bg-accent/20 hover:scale-110' size={48} />}
+      <div className="w-full bg-foreground pb-6 border border-accent/10 rounded-xl px-6 pt-4 transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-accentHighlight">
+        <div className='flex flex-row gap-x-4 items-center'>
+          {Icon && <Icon className='text-accent bg-accent/20 rounded-2xl p-3 transition duration-200 hover:bg-accent/5 hover:scale-110' size={56} />}
           <p className='text-2xl font-semibold'>{title}</p>
         </div>
-        <p className='text-lg mt-3'>{description}</p>
+        <p className='text-lg mt-4'>{description}</p>
       </div>
     </div>
   )
