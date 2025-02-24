8c4bdf24968a6aa6cf0fedcb40e64b3f07af4042
Krishab Bashyal <krishab2002@gmail.com>
2025-02-24 16:58:38 -0600

    Added easier toggle for devtool

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+// import BreakpointInfo from "../components/DevTools/BreakpointInfo";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body className="min-h-screen bg-background text-white">
-
+        {/* <BreakpointInfo /> */}
         {children}
       </body>
     </html>
