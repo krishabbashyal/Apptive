3700d6316a09e95a2b367869a1e3efa17b743e92
Krishab Bashyal <krishab2002@gmail.com>
2025-02-24 15:17:57 -0600

    updates to get state value from seach bar

--- client/src/components/UserInterface/RecentArchive.tsx
+++ client/src/components/UserInterface/RecentArchive.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Application } from '@prisma/client'
 import SearchBar from './SearchBar'
 import CustomButton from './CustomButton'
@@ -11,13 +11,24 @@ interface RecentArchiveProps {
 }
 
 function RecentArchive({applicationData}: RecentArchiveProps) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredData = applicationData.filter(application =>
+    application.company_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    application.location_city.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    application.location_state.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    application.job_title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    application.location_city.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    application.application_status.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="h-full rounded-tr-xl rounded-tl-xl bg-foreground">
     <div className="mx-10 py-6">
       <div className="flex flex-row justify-between">
         <h1 className="text-3xl">Archived Applications</h1>
         <div className="flex flex-row gap-x-4">
-          <SearchBar />
+          <SearchBar onSearchChange={setSearchTerm} />
           <CustomButton
             onClick={() => console.log("Create button pressed")}
             label="Create"
@@ -26,7 +37,7 @@ function RecentArchive({applicationData}: RecentArchiveProps) {
         </div>
       </div>
       <div className="mt-8">
-        <ArchivedApplicationTable applicationData={applicationData} />
+        <ArchivedApplicationTable applicationData={filteredData} />
       </div>
     </div>
   </div>
