e80a0e27af20ff90c58b0e2241379b3fc3c640b0
Krishab Bashyal <krishab2002@gmail.com>
2025-02-24 19:35:59 -0600

    Started working on the map page

--- client/src/app/(main)/map/page.tsx
+++ client/src/app/(main)/map/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import ApplicationMap from "@/components/UserInterface/ApplicationMap";
+import LocationLeaderboard from "@/components/UserInterface/LocationLeaderboard";
 
 const Map = () => {
   return (
-    <div>Map Page</div>
-  )
-}
+    <div className="mx-12 h-full pt-8">
+      <div className="h-full rounded-tl-xl rounded-tr-xl bg-foreground">
+        <div className="mx-10 h-full py-6">
+          <div className="flex flex-row justify-between">
+            <h1 className="text-3xl font-semibold text-gray-100">
+              Desired Locations
+            </h1>
+          </div>
+          <div className="mt-8">
+            <div className="flex h-full w-full flex-row gap-x-8">
+              <ApplicationMap />
+              <LocationLeaderboard />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
