7cb562a7a72668f7fb2e06cb9b9a03e4078947af
Krishab Bashyal <krishab2002@gmail.com>
2025-02-27 16:15:39 -0600

    Made changes to startcard to remove repeating decimals

--- client/src/components/UserInterface/StatCard.tsx
+++ client/src/components/UserInterface/StatCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 interface StatCardProps {
-  stat: number | string
+  stat: number
   description: string
 }
 
@@ -9,7 +9,7 @@ const StatCard = ({stat, description}: StatCardProps) => {
   return (
     <div className='flex flex-col flex-grow bg-foreground rounded-xl pl-6 text-left hover:text-accent hover:ring-[2px] hover:ring-accent/25'>
       <p className='font-medium text-lg mt-3 '>{description}</p>  
-      <p className='text-4xl font-semibold mt-3 pb-4'>{description === 'Interview Rate' ? stat + '%' : stat}</p>
+      <p className='text-4xl font-semibold mt-3 pb-4'>{description === 'Interview Rate' ? Math.round(stat * 100) / 100  + '%' : stat}</p>
     </div>
   )
 }
